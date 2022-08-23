4099acb78efe9a8d0b83302ab1655ca3b0468747
Ivan <375291073707ls@gmail.com>
2022-08-23 20:49:33 +0300

    Задача 64 с решением

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+M = 1; N = 9. -> "3, 6, 9" M = 13; N = 20. -> "15, 18"*/
+
+
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
